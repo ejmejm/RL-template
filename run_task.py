83bc49f32e6e712e9244ad23f6c92234fb4a3044
ejmejm <ninjaedan@aol.com>
2019-07-05 11:07:35 -0700

    first commit

---
+++ run_task.py
@@ -0,0 +1,107 @@
+import numpy as np
+import gym
+import time
+from mpi4py import MPI
+import heapq
+import tensorflow as tf
+from model import Model
+from utils import *
+import multiprocessing
+import pickle
+
+
+def worker(model, max_steps=1000):
+    """
+    Performs the game simulation, and is called across all processes.
+    Returns a list of training data in the shape (n_steps, 4), and
+    and the total reward of the rollout.
+    """
+    train_data = []
+    env = make_env()
+    obs = env.reset()
+    # obs = filter_obs(obs)
+
+    ep_reward = 0
+    for _ in range(max_steps):
+        act, val = model.gen_actions_and_values([obs])
+        act, val = act[0], val[0]
+
+        next_obs, rew, d, _ = env.step(act)
+        # next_obs = filter_obs(next_obs)
+        train_data.append([obs, act, rew, val, next_obs])
+        obs = next_obs
+        ep_reward += rew
+        if d:
+            break
+
+    train_data = np.asarray(train_data)
+
+    ep_reward = np.sum(train_data[:, 2])
+    # Discount rewards
+    train_data[:, 2] = discount_rewards(train_data[:, 2])
+
+    return train_data, ep_reward
+
+
+if __name__ == '__main__':
+    ### Setp for MPI ###
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    n_processes = comm.Get_size()
+    controller = 0
+
+    ### Define starting parameters ###
+    n_epochs = 1000
+    n_train_batches = 32
+    n_process_batches = int(n_train_batches / n_processes)
+    log_freq = 5
+    init_logger('training.log')
+
+    ### Enable gpu usage for just the main process for training ###
+    if rank == controller:
+        device_config = tf.ConfigProto()
+    else:
+        device_config = tf.ConfigProto(device_count={'GPU': 0})
+
+    model = Model(comm, controller, rank, n_acts=2,
+                  obs_shape=(4,), sess_config=device_config)
+
+    all_rewards = []
+    for epoch in range(1, n_epochs+1):
+        batch_data = []
+        train_data = []
+        for _ in range(n_process_batches):
+            ### Simulate more episodes to gain training data ###
+            if rank == controller:
+                batch_data = comm.gather(worker(model), controller)
+
+                batch_train_data = [datum[0] for datum in batch_data]
+                batch_reward_data = [datum[1] for datum in batch_data]
+
+                train_data.extend(batch_train_data)
+                all_rewards.extend(batch_reward_data)
+            else:
+                comm.gather(worker(model), controller)
+
+        if rank == controller:
+            ### Log and print reward ###
+            if epoch % log_freq == 0:
+                print(
+                    f'Epoch: {epoch}, Avg Reward: {np.mean(all_rewards[-n_train_batches:])}')
+                log(
+                    f'Epoch: {epoch}, Avg Reward: {np.mean(all_rewards[-n_train_batches:])}')
+
+            ### Format training data ###
+            train_data = np.concatenate(train_data)
+            np.random.shuffle(train_data)
+
+            obs_train_data = np.vstack(train_data[:, 0])
+            action_train_data = train_data[:, 1]
+            reward_train_data = train_data[:, 2]
+
+            ### Train model and sync weights with all processes ###
+            model.train_policy(
+                obs_train_data, action_train_data, reward_train_data)
+            model.sync_weights()
+        else:
+            model.sync_weights()
---
+++ utils.py
@@ -0,0 +1,45 @@
+import cv2
+import gym
+import numpy as np
+
+def init_logger(lp):
+    global log_path
+    log_path = lp
+
+    f = open(log_path, 'w+')
+    f.close()
+
+def log(string):
+    with open(log_path, 'a') as f:
+        f.write(string + '\n')
+
+def make_env():
+    return gym.make('CartPole-v0')
+
+def filter_obs(obs, obs_shape=(42, 42)):
+    """
+    Used for formatting 2D observations.
+    Filters an obs to the right size, color, and scale.
+    Only works for 1 obs at a time.
+    """
+    assert(type(obs) == np.ndarray), "The observation must be a numpy array!"
+    assert(len(obs.shape) == 2), "The observation must be a 2D array!"
+
+    obs = cv2.resize(obs, obs_shape, interpolation=cv2.INTER_LINEAR)
+    obs = cv2.cvtColor(obs, cv2.COLOR_BGR2GRAY)
+    obs = obs / 255.
+    return obs
+
+def discount_rewards(rewards, gamma=0.99):
+    new_rewards = [float(rewards[-1])]
+    for i in reversed(range(len(rewards)-1)):
+        new_rewards.append(float(rewards[i]) + gamma * new_rewards[-1])
+    return np.array(new_rewards[::-1])
+
+def calculate_gaes(rewards, values, gamma=0.99, decay=0.95):
+    next_values = values[1:] + [0]
+    deltas = [rew + gamma * next_val - val for rew, val, next_val in zip(rewards, values, next_values)]
+    gaes = [deltas[-1]]
+    for i in reversed(range(len(deltas)-1)):
+        gaes.append(deltas[i] + decay * gamma * gaes[-1])
+    return np.array(gaes[::-1])
